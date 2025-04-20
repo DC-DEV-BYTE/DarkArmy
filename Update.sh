953d2298f8ea28e66f5e33c5f56d445e2438ddf6
Danny Brodeur <Dannybrodeurcote1983@gmail.com>
2025-04-19 22:50:22 -0400

    Create Update.sh

---
+++ Update.sh
@@ -0,0 +1,54 @@
+#
+#     ██████   █████  ██████  ██   ██  █████  ██████  ███    ███ ██    ██ 
+#     ██   ██ ██   ██ ██   ██ ██  ██  ██   ██ ██   ██ ████  ████  ██  ██  
+#     ██   ██ ███████ ██████  █████   ███████ ██████  ██ ████ ██   ████   
+#     ██   ██ ██   ██ ██   ██ ██  ██  ██   ██ ██   ██ ██  ██  ██    ██    
+#     ██████  ██   ██ ██   ██ ██   ██ ██   ██ ██   ██ ██      ██    ██    "
+#
+#                          ~@~ code by 0X4ritra ~@~
+
+clear
+
+sudo chmod +x /etc/
+
+clear
+
+sudo chmod +x /usr/share/doc
+
+clear
+
+sudo rm -rf /usr/share/doc/DARKARMY/
+
+clear
+
+cd /etc/
+
+clear
+
+sudo rm -rf /etc/DARKARMY
+
+clear
+
+mkdir DARKARMY
+
+clear
+
+cd DARKARMY
+
+clear
+
+git clone https://github.com/0x4ritra/DARKARMY.git
+
+clear
+
+cd DARKARMY
+
+clear
+
+sudo chmod +x install.sh
+
+clear
+
+./install.sh
+
+clear
